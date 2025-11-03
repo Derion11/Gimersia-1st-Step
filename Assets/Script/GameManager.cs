3937987cc128fb2d97dcd1806bebcd98f0fc197f
Derion_11 <dy110703@gmail.com>
2025-11-03 17:19:07 +0700

    Add project files.

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private void Awake()
+    {
+        Debug.Log("Calling Awake...");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Calling Start...");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Calling Update...");
+    }
+}
---
+++ Assets/Script/Testing.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+    [SerializeField] SpriteRenderer spRenderer;
+    private void Start()
+    {
+        if (spRenderer != null)
+        {
+            Debug.Log($"SpriteRenderer{spRenderer.sprite.name}");
+        }
+        else
+        {
+            Debug.Log("SpriteRenderer tidak ada");
+        }
+    }
+    private void Update()
+    {
+        Debug.Log("Calling Update...");
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Space key was pressed.");
+            if (spRenderer != null)
+            {
+                spRenderer.color = Color.red;
+            }
+            else
+            {
+                Debug.Log("SpriteRenderer tidak ada");
+            }
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            Debug.Log("Space key was pressed.");
+            if (spRenderer != null)
+            {
+                spRenderer.color = Color.green;
+            }
+            else
+            {
+                Debug.Log("SpriteRenderer tidak ada");
+            }
+        }
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    Debug.Log("Space key was pressed.");
+        //    if (spRenderer != null)
+        //    {
+        //        spRenderer.color = Color.blue;
+        //    }
+        //    else
+        //    {
+        //        Debug.Log("SpriteRenderer tidak ada");
+        //    }
+        //}
+    }
+}
