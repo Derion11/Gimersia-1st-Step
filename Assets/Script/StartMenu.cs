8253669716b4d7bd2758d7c84dcf584a4b1b8ee4
Derion_11 <dy110703@gmail.com>
2025-11-10 23:25:19 +0700

    Added main_menu_theme. And start button sound.

--- Assets/Script/StartMenu.cs
+++ Assets/Script/StartMenu.cs
@@ -5,9 +5,33 @@ using UnityEngine.SceneManagement;
 
 public class StartMenu : MonoBehaviour
 {
-    public void ChangeScene()
+    void Start()
     {
-        SceneManager.LoadScene(1);
+        // Panggil BGM menggunakan NAMA FILENYA
+        if (AudioManager.Instance != null)
+        {
+            AudioManager.Instance.PlayBGM("main_menu_theme");
+            // ^ Ganti "MainTheme" jika Anda menggunakan "CasualTheme"
+        }
     }
 
+    // 2. MODIFIKASI FUNGSI INI
+    public void StartGame()
+    {
+        // Panggil SFX menggunakan NAMA FILENYA
+        if (AudioManager.Instance != null)
+        {
+            AudioManager.Instance.PlaySFX("button_push");
+            AudioManager.Instance.StopBGM();
+        }
+
+        // Kode Anda yang sudah ada
+        SceneManager.LoadScene(1); // Ganti jika perlu
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
 }
